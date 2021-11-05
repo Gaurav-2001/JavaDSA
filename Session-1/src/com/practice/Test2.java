9feecbe404429ef55918dfd7ec24b7df876adae3
Gaurav Pagare <gauravpagare2001@gmail.com>
2021-11-05 07:27:42 +0530

    Input/output Debugging and Datatype

--- Session-1/src/com/practice/Test2.java
+++ Session-1/src/com/practice/Test2.java
@@ -1,5 +1,15 @@
+//Temp. Conversion Program
 package com.practice;
 
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
 public class Test2 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter the Temprature in C : ");
+        float temp = input.nextFloat();
+        double newTemp = (temp*1.8) + 32;
+        System.out.print("\nTemprature in Faranite is : " + newTemp);
+    }
 }
