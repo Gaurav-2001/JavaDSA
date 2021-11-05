ed4826641a06cccccf942362aa9635e0443ff7b2
Gaurav Pagare <gauravpagare2001@gmail.com>
2021-11-05 06:37:00 +0530

    Input/output Debugging and Datatype

--- Session-1/src/com/gaurav/Main.java
+++
@@ -1,8 +0,0 @@
-package com.gaurav;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ Session-1/src/com/practice/Test1.java
@@ -0,0 +1,7 @@
+package com.practice;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello this is a test program 1");
+    }
+}
